5e73b6328819a511041b624243f688371f7c607f
Stas Pasynkov <s.pasynkov@gmail.com>
2020-02-07 05:23:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/crimeintent/CrimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.crimeintent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CrimeActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
+	}
+}
---
+++ app/src/main/java/com/example/crimeintent/CrimeFragment.java
@@ -0,0 +1,6 @@
+package com.example.crimeintent;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeFragment extends Fragment {
+}
